4a484a2f5dfbf977ca26558a6ed35bf5fdcd0394
Corno Schraverus <corno@schraverus.com>
2025-12-29 10:07:51 +0100

    first commit

---
+++ pub/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * these functions coming from core-internals should be exposed for serializer development
+ */
+export {
+    build_text,
+    build_list,
+    panic as unreachable_code_path,
+    integer_division,
+    text_to_character_list,
+} from "pareto-core-internals"
+
