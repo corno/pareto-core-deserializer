3a68fd3e41b4dc4ee9f3acf20cb939390a57fc9d
Corno Schraverus <corno@schraverus.com>
2025-12-29 10:20:32 +0100

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -2,10 +2,11 @@
  * these functions coming from core-internals should be exposed for serializer development
  */
 export {
-    build_text,
+    // build_text, probably not needed
     build_list,
     panic as unreachable_code_path,
     integer_division,
     text_to_character_list,
+    create_iterator,
 } from "pareto-core-internals"
 
